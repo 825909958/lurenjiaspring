56b96770d2a8a0392bab76e1a632cb0b49e74750
THT <825909958@qq.com>
2023-10-23 20:39:08 +0800

    2023-10-23

--- lurenjia/src/test/java/com/example/lurenjiaspring/LurenjiaspringApplicationTests.java
+++ lurenjia/src/test/java/com/example/lurenjiaspring/LurenjiaspringApplicationTests.java
@@ -1,13 +1,33 @@
 package com.example.lurenjiaspring;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootTest
+import java.util.Arrays;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest(classes = LurenjiaspringApplication.class)
+//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class LurenjiaspringApplicationTests {
+    @Value("${name}")
+    private String name;
+    //@Value("${spring.name}")
+    //private String sName;
 
     @Test
-    void contextLoads() {
+    public void a() {
+        System.out.println("name = " + name);
+        //System.out.println("sName = " + sName);
+    }
+
+    @Test
+    public void noSpringEnvi() {
+        AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(LurenjiaspringApplication.class);
+        String[] beanDefinitionNames = context.getBeanDefinitionNames();
+        Arrays.stream(beanDefinitionNames).forEach(System.out::println);
+        //ClassPathXmlApplicationContext xmlApplicationContext = new ClassPathXmlApplicationContext();
     }
 
 }
